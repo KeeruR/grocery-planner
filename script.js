bc6d9ac63c5d312e1c0065ad9f7226931bd76237
Keeru Rangu <keerurangu@icloud.com>
2025-05-06 22:14:44 +1200

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ function toggleItem(index) {
 
 document.getElementById("refreshList").addEventListener("click", function () {
   if (confirm("Are you sure you want to clear your grocery list?")) {
-    localStorage.removeItem("groceryList"); // Remove from storage
-    document.getElementById("grocery-list").innerHTML = ""; // Clear the displayed list
+    localStorage.removeItem("groceryList"); 
+    document.getElementById("grocery-list").innerHTML = ""; 
     alert("Grocery list cleared!");
   }
 });
